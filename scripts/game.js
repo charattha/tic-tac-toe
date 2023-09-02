bf16bb86a8c6bab9d4b86b40d368d847cae8d118
Ratthawit Lekwongderm <129240869+charattha@users.noreply.github.com>
2023-09-02 12:34:09 +0700

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -74,31 +74,38 @@ function checkForGameOver() {
     for (let i = 0; i < 3; i++) {
         if (gameData[i][0] > 0 &&
             gameData[i][0] === gameData[i][1] &&
-            gameData[i][1] === gameData[i][2]) {
+            gameData[i][1] === gameData[i][2]
+        ) {
             return gameData[i][0];
         }
     }
+
     // Check the column for equality
     for (let i = 0; i < 3; i++) {
         if (gameData[0][i] > 0 &&
             gameData[0][i] === gameData[1][i] &&
-            gameData[0][i] === gameData[2][i]) {
-            return gameData[i][0];
+            gameData[0][i] === gameData[2][i]
+        ) {
+            return gameData[0][i];
         }
     }
 
     // Diagobal : Top left to buttom right
     if (gameData[0][0] > 0 &&
         gameData[0][0] === gameData[1][1] &&
-        gameData[1][1] === gameData[2][2]) {
+        gameData[1][1] === gameData[2][2]
+    ) {
         return gameData[0][0];
     }
+
     // Diagobal : Buttom left to top right
     if (gameData[2][0] > 0 &&
         gameData[2][0] === gameData[1][1] &&
-        gameData[1][1] === gameData[0][2]) {
+        gameData[1][1] === gameData[0][2]
+    ) {
         return gameData[2][0];
     }
+
     if (currentRound === 9) {
         return -1;
     }
